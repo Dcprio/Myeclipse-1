ecb82c2c1d5102c98255130b9afa8792220f9e98
SongGangs <sg951205@vip.qq.com>
2017-04-25 16:12:36 +0800

    第一次测试myeclipse

---
+++ HelloGitHub/src/TestClass.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+
+/**
+ * @Title:TestClass
+ * @Description:
+ * @Personal
+ * @author : SGang
+ * @date : 2017Äê4ÔÂ25ÈÕ
+ */
+public class TestClass {
+	public static void main(String[] args) {
+		System.out.println("sssssssss");
+	}
+}
